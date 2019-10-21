1f159784e1e0a28a79b81d037ac2dae6dd9608c1
fabio <fabiofanni@users.noreply.github.com>
2019-10-21 09:38:46 -0700

    Add files via upload

---
+++ twitter_quote-gen_20191021.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+#initi block
+#!/usr/bin/env python
+# encoding: utf-8
+
+import tweepy #https://github.com/tweepy/tweepy
+import csv
+import json
+from time import sleep
+import pandas as pd
+import time
+import datetime as dt
+
+#fill with Twitter API credentials
+credentials={}
+credentials['consumer_key']=''
+credentials['consumer_secret']= ''
+credentials['access_token']=''
+credentials['access_token_secret']= ''
+
+# Save the credentials object to file
+with open("twitter_credentials.json", "w") as file:
+    json.dump(credentials, file)
+
+#authorize twitter, initialize tweepy
+auth = tweepy.OAuthHandler(credentials['consumer_key'], credentials['consumer_secret'])
+auth.set_access_token(credentials['access_token'], credentials['access_token_secret'])
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+#read csv with quotes
+df = pd.read_csv('quotes.csv')
+
+#process dataframe
+df['all_quote'] = df['Quote'] + ' -' + df['Author']
+df.drop(['Quote','Author'], axis=1)
+pd.options.display.max_colwidth = 200
+df = df.dropna()
+
+
+# In[16]:
+
+
+time_interval = 10
+
+while True:
+    quote = (df['all_quote'].sample(n = 1)).to_string()
+    quote = quote.lstrip("0123456789=,")
+    api.update_status(quote)
+    time.sleep(time_interval)
+
+
+# In[ ]:
+
+
+
+
